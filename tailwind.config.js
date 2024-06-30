0ba05fd909c6fccec4f610e1cb973b9678766393
chowjustin <justin.chow05@gmail.com>
2024-07-01 00:17:14 +0700

    feat: add Poppins font and purple colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        purple: {
+          100: "#f3e8ff",
+          200: "#d3bff8",
+          300: "#b097f1",
+          400: "#9c7ce5",
+          500: "#8b5cf6",
+          600: "#7c3aed",
+          700: "#6d28d9",
+          800: "#5b21b6",
+          900: "#4c1d95",
+        },
+      },
+    },
   },
   plugins: [],
 };
